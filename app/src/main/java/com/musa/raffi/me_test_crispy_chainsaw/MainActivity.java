aba4024a757368e25e9ee2753bd74fe03bcace59
aunorafiq <raffi.musa07@gmail.com>
2016-09-28 20:24:54 +0700

    new project

---
+++ app/src/main/java/com/musa/raffi/me_test_crispy_chainsaw/MainActivity.java
@@ -0,0 +1,13 @@
+package com.musa.raffi.me_test_crispy_chainsaw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
